6f7cf23a4f5191625eb5d5801453faa6de4bac3f
Steffen Johansen <idontparty@users.noreply.github.com>
2022-09-23 16:34:21 +0200

    Fix typos, added parent dir creation feature.

--- tt.sh
+++ tt.sh
@@ -13,24 +13,27 @@ CORE_DEV="konsole neovim zsh tmux powerline"
 
 echo '[!] Installing packages. Requires sudo privs.'
 # Install basic toolkit
-if [ "$DISTRO" == "ARCH" ]; then
+if [[ "$DISTRO" == "ARCH" ]]; then
   sudo pacman -S "$CORE_DEV" "noto-fonts-emoji"
-elif [ "$DISTRO" == "DEB"]; then
+elif [[ "$DISTRO" == "DEB" ]]; then
   sudo apt update && sudo apt upgrade
   sudo apt install "$CORE_DEV" "fonts-powerline"
 fi
 
 # Oh my zsh from github
+
+OMZPATH='/tmp/ohmyzsh.sh'
 echo '[!] Downloading ohmyzsh. Do you want to read through source? [yn]'
-wget 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh'
+wget -O "$OMZPATH" 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh'
 
 while true; do
     read -p "[?] Do you want to read through the source code? [Yn]" yn
     case "$yn" in
         [Nn]* ) echo 'Ignoring safety concerns.. GG.'; break;;
-        * ) less 'install.sh'; break;;
+        * ) less "$OMZPATH"; break;;
     esac
 done
+bash "$OMZPATH"
 
 # Tmux Plugin manager from github
 echo '[!] Installing Tmux Plugin Manager (TPM).'
@@ -45,13 +48,21 @@ LIBDIR="$PWD/lib/"
 # Gitconfig
 ln -sf "$LIBDIR/gitconfig" "$HOME/.gitconfig"
 # Konsole colorscheme
-ln -sf "$LIBDIR/Nordic.colorscheme" "$HOME/.local/share/konsole/Nordi.colorscheme"
+KONSOLE_COLORSCHEME_DIR="$HOME/.local/share/konsole"
+mkdir -p $KONSOLE_COLORSCHEME_DIR
+ln -sf "$LIBDIR/Nordic.colorscheme" "$KONSOLE_COLORSCHEME_DIR/Nordic.colorscheme"
 # Konsole config
-ln -sf "$LIBDIR/konsolerc" "$HOME/.config/konsolerc"
+KONSOLE_RC_DIR="$HOME/.config"
+mkdir -p "$KONSOLE_RC_DIR"
+ln -sf "$LIBDIR/konsolerc" "$KONSOLE_RC_DIR/konsolerc"
 # Konsole profile
-ln -sf "$LIBDIR/kpro.profile" "$HOME/.local/share/konsole/kpro.profile"
+KONSOLE_PROFILE_DIR="$KONSOLE_COLORSCHEME_DIR"
+mkdir -p "$KONSOLE_PROFILE_DIR"
+ln -sf "$LIBDIR/kpro.profile" "$KONSOLE_PROFILE_DIR/kpro.profile"
 # NVIM config
-ln -sf "$LIBDIR/init.vim" "$HOME/.config/nvim/init.vim"
+NVIM_CONFIG_DIR="$HOME/.config/nvim"
+mkdir -p "$NVIM_CONFIG_DIR"
+ln -sf "$LIBDIR/init.vim" "$NVIM_CONFIG_DIR/init.vim"
 # tmux.conf
 ln -sf "$LIBDIR/tmux.conf" "$HOME/.tmux.conf"
 # zshrc
