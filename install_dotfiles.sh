8190fd9682ed21f918d4a5bbb854bddcbaaf7738
Steffen Johansen <idontparty@users.noreply.github.com>
2022-12-28 09:25:40 +0100

    refactored the dotfiles repo and simplified

---
+++ install_dotfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# zshrc
+ln -s "$PWD/zshrc" "$HOME/.zshrc"
+
+# vimrc
+ln -s "$PWD/vimrc" "$HOME/.vimrc"
+
+# gitconfig
+ln -s "$PWD/gitconfig" "$HOME/.gitconfig"
+
+# tmux.conf
+ln -s "$PWD/tmux.conf" "$HOME/.tmux.conf"
---
+++ vimrc
@@ -0,0 +1,10 @@
+" Vim config
+"
+" Set terminal gui colors.
+set termguicolors
+
+" Set line number
+set number
+
+" Use shared clipboard.
+set clipboard+=unnamedplus
